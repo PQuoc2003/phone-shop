816d86f4e9e0bb647d539bcae8a635f5f5292c81
Phú Quốc <dinhphuquoc2003@gmail.com>
2024-11-15 09:58:03 +0700

    modify html 50% ~~

--- phone-commerce/src/main/java/com/tdtu/phonecommerce/controller/MyErrorController.java
+++ phone-commerce/src/main/java/com/tdtu/phonecommerce/controller/MyErrorController.java
@@ -1,13 +1,21 @@
 package com.tdtu.phonecommerce.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
+@ControllerAdvice
 public class MyErrorController implements ErrorController {
 
-    @RequestMapping(value = "/error")
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public String getRedirect(){
-        return "redirect:/home";
+        return "404";
     }
 
 }
